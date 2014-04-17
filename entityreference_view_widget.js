a59204390170ee69571d9ef1fefc1577f435e71e
jsacksick <jonathan.sacksick@gmail.com>
2014-04-17 02:41:22 +0200

    Issue #2242819 by jsacksick: Make use of Ajax Form API for the modal.

--- entityreference_view_widget.js
+++ entityreference_view_widget.js
@@ -1,67 +1,74 @@
 (function($) {
-Drupal.behaviors.entityreferenceViewWidget = {
-  attach: function(context, settings) {
-    $('.ervw-add-items').bind('click',
-      function() {
-        if (typeof Drupal.settings['views'] != 'undefined') {
-          Drupal.settings['views']['ajaxViews'] = null;
-        }
-      }
-    );
-    var checkboxes = '#modal-content input[name="entity_ids[]"]';
-    $('#entityreference-view-widget-select-all').unbind('click').text('Select all').data('unselect', 0).click(function(){
-      if ($(this).data('unselect')) {
-        $(checkboxes).removeAttr('checked');
-        $(this).data('unselect', 0).text('Select all');
-      }
-      else {
-        $(checkboxes).attr('checked', 'checked');
-        $(this).data('unselect', 1).text('Unselect all');
-      }
-      return false;
-    });
-    $('#entityreference-view-widget-modal-submit .button').click(function(){
-      $('#modal-content .error').remove();
-      var selected_amount = $(checkboxes + ':checked').length;
-      var settings_selector = '#' + $('#entityreference-view-widget-settings-selector').val();
-      var widget_settings = JSON.parse($(settings_selector).val());
-      var entity_ids = $(checkboxes).serialize();
-      var query_string = entity_ids + '&widget_settings=' + $(settings_selector).val();
-
-      $('#' + widget_settings.table_id + ' input[type=checkbox]:checked').each(function(){
-        query_string += '&default_entity_ids[' + $(this).data('delta') + ']=' + $(this).val();
-        selected_amount++;
-      });
-
-      if (widget_settings.cardinality > 0 && widget_settings.cardinality < selected_amount) {
-        $('#modal-content').prepend('<div class="messages error">Please select no more than ' + widget_settings.cardinality + ' values.</div>');
-      }
-      else {
-        $.ajax({
-          url: Drupal.settings.basePath + '?q=entityreference_view_widget/ajax',
-          type: 'POST',
-          dataType: 'html',
-          data: query_string,
-          success: function(data) {
-            data && $('#' + widget_settings.table_id + ' tbody').html(data);
-            $('#' + widget_settings.table_id + ' tbody tr').each(function(){
-              var el = $(this);
-              if (widget_settings.cardinality !== '1') {
-                Drupal.tableDrag[widget_settings.table_id].makeDraggable(el.get(0));
-                el.find('td:last').addClass('tabledrag-hide');
-                if ($.cookie('Drupal.tableDrag.showWeight') == 1) {
-                  el.find('.tabledrag-handle').hide();
-                }
-                else {
-                  el.find('td:last').hide();
-                }
-              }
-            });
-            widget_settings.close_modal && Drupal.CTools.Modal.dismiss();
+  Drupal.behaviors.entityreferenceViewWidget = {
+    attach: function(context, settings) {
+      // The following code is a workaround to make the eventual exposed filters
+      // work on the embedded view displayed in the modal, probably remove once
+      // http://drupal.org/node/1809958 has been merged.
+      $('.ervw-add-items').bind('click',
+        function() {
+          if (typeof Drupal.settings['views'] != 'undefined') {
+            Drupal.settings['views']['ajaxViews'] = null;
           }
+        }
+      );
+      $('.entityreference-view-widget-modal-submit').each(function() {
+        var selected_ids = [];
+        var selector = '#' + $(this).data('table-id') + ' input[type=checkbox]:checked';
+        $(selector).each(function(i) {
+          selected_ids[i] = $(this).val();
         });
+        if (selected_ids) {
+          $(this).attr('data-selected-ids', selected_ids.join(';'));
+        }
+      });
+      var checkboxes = '#modal-content input[name="entity_ids[]"]';
+      $('#entityreference-view-widget-select-all').unbind('click').text('Select all').data('unselect', 0).click(function() {
+        if ($(this).data('unselect')) {
+          $(checkboxes).removeAttr('checked');
+          $(checkboxes).trigger('change');
+          $(this).data('unselect', 0).text('Select all');
+        }
+        else {
+          $(checkboxes).attr('checked', 'checked');
+          $(checkboxes).trigger('change');
+          $(this).data('unselect', 1).text('Unselect all');
+        }
+        return false;
+      });
+      // Maintain an entity_ids hidden field on the form that is displayed at the
+      // bottom of the modal, this is used by the ajax callback to append the
+      // selected values.
+      var entity_ids_selector = '#ervw-modal-form-entity-ids';
+      $(checkboxes).bind('change', function() {
+          if ($(entity_ids_selector).parent().attr('data-selected-ids')) {
+            $(entity_ids_selector).val($(entity_ids_selector).parent().data('selected-ids'));
+          }
+          $(checkboxes + ':checked').each(function(i) {
+              var el = $(this);
+              $('#ervw-modal-form-entity-ids').val(function(index, value) {
+                  return value + ';' + el.val();
+                }
+              )
+            }
+          );
+        }
+      );
+    }
+  }
+
+  // Create a new ajax command, ervw_draggable that is called to make the rows
+  // of the widget draggable.
+  Drupal.ajax.prototype.commands.ervw_draggable = function(ajax, response, status) {
+    $('#' + response.selector + ' tr').each(function(){
+      var el = $(this);
+      Drupal.tableDrag[response.selector].makeDraggable(el.get(0));
+      el.find('td:last').addClass('tabledrag-hide');
+      if ($.cookie('Drupal.tableDrag.showWeight') == 1) {
+        el.find('.tabledrag-handle').hide();
+      }
+      else {
+        el.find('td:last').hide();
       }
     });
-  }
-}
+  };
 })(jQuery);
