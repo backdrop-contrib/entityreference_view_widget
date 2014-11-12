d5d76e4352bdcd6417302c216fb28090fc44501c
jsacksick <jonathan.sacksick@gmail.com>
2014-11-12 11:01:02 +0100

    Commit work in progress.

--- src/Form/ModalForm.php
+++ src/Form/ModalForm.php
@@ -7,11 +7,14 @@
 
 namespace Drupal\entity_reference_view_widget\Form;
 
-use Drupal\Component\Utility\Html;
-use Drupal\Core\Ajax\AlertCommand;
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CloseModalDialogCommand;
+use Drupal\Core\Ajax\PrependCommand;
+use Drupal\Core\Ajax\ReplaceCommand;
+use Drupal\Core\Ajax\RemoveCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\entity_reference_view_widget\Plugin\Field\FieldWidget\ViewWidget;
 
 /**
  * Provides the database logging filter form.
@@ -31,6 +34,7 @@ class ModalForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $build_info = $form_state->getBuildInfo();
     $settings = $build_info['args'][0];
+    $form_state->set('ervw_settings', $settings['settings']);
     $form['view'] = array(
       '#markup' => drupal_render($settings['preview']),
     );
@@ -53,7 +57,26 @@ class ModalForm extends FormBase {
    */
   public function addItemsAjax(array $form, FormStateInterface &$form_state) {
     $response = new AjaxResponse();
-    $response->addCommand(new AlertCommand('ok'));
+    $input = $form_state->getUserInput();
+    $messages = drupal_get_messages('error', FALSE);
+
+    if (!empty($messages)) {
+      $status_messages = array('#theme' => 'status_messages', '#display' => 'error');
+      $response->addCommand(new RemoveCommand('#drupal-modal .messages'));
+      $response->addCommand(new PrependCommand('#drupal-modal', drupal_render($status_messages)));
+    }
+    else {
+      if (!empty($input['entity_ids'])) {
+        $settings = $form_state->get('ervw_settings');
+        $rows = ViewWidget::getRows($input['entity_ids'], $settings);
+        $response->addCommand(new ReplaceCommand('#' . $settings['wrapper'] . ' > tbody', '<tbody>' . drupal_render($rows) . '</tbody>'));
+
+        // Close the modal if the widget setting has been selected.
+        if (!empty($settings['close_modal'])) {
+          $response->addCommand(new CloseModalDialogCommand());
+        }
+      }
+    }
 
     return $response;
   }
@@ -62,6 +85,14 @@ class ModalForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    $settings = $form_state->get('ervw_settings');
+    $input = $form_state->getUserInput();
+
+    if (!empty($input['entity_ids'])) {
+      if ($settings['cardinality'] > 0 && $settings['cardinality'] < count($input['entity_ids'])) {
+        drupal_set_message(t('Please select no more than @cardinality values', array('@cardinality' => $settings['cardinality'])), 'error');
+      }
+    }
   }
 
   /**
--- src/Plugin/Field/FieldWidget/ViewWidget.php
+++ src/Plugin/Field/FieldWidget/ViewWidget.php
@@ -7,11 +7,13 @@
 
 namespace Drupal\entity_reference_view_widget\Plugin\Field\FieldWidget;
 
+use Drupal\Component\Utility\Html;
 use Drupal\Component\Utility\String;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\OpenModalDialogCommand;
 use Drupal\Core\Ajax\SettingsCommand;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Views;
@@ -26,7 +28,8 @@ use Drupal\views\Views;
  *   description = @Translation("An advanced, view-based widget."),
  *   field_types = {
  *     "entity_reference"
- *   }
+ *   },
+ *   multiple_values = TRUE
  * )
  */
 class ViewWidget extends WidgetBase {
@@ -108,11 +111,80 @@ class ViewWidget extends WidgetBase {
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $field_name = $this->fieldDefinition->getName();
+    $cardinality = $this->fieldDefinition->getFieldStorageDefinition()->getCardinality();
+    $parents = $form['#parents'];
+
+    // Determine the number of widgets to display.
+    switch ($cardinality) {
+      case FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED:
+        $field_state = static::getWidgetState($parents, $field_name, $form_state);
+        $max = $field_state['items_count'];
+        $is_multiple = TRUE;
+        break;
+
+      default:
+        $max = $cardinality - 1;
+        $is_multiple = ($cardinality > 1);
+        break;
+    }
+    /*$element += array(
+      '#title' => $title,
+      '#description' => $description,
+      '#markup' => t('No items had been added yet. Click "Add items" to launch the widget.'),
+    );
+    $element['add_more'] = array(
+      '#type' => 'button',
+      '#value' => t('Add items'),
+      '#ajax' => array(
+        'callback' => array($this, 'ajaxCallback'),
+      ),
+      '#limit_validation_errors' => array(),
+    );*/
+    $id_prefix = implode('-', array_merge($parents, array($field_name)));
+    $wrapper_id = Html::getUniqueId($id_prefix . '-ervw-wrapper');
     $element += array(
-      '#type' => 'textfield',
+      '#theme' => 'field_multiple_value_form',
+      '#field_name' => $field_name,
+      '#cardinality' => $cardinality,
+      '#cardinality_multiple' => TRUE,
+      '#required' => $this->fieldDefinition->isRequired(),
+      '#max_delta' => $max,
     );
 
+    if (!$items->isEmpty()) {
+
+    }
+    else {
+      $element[0]['#markup'] = t('No items had been added yet. Click "Add items" to launch the widget.');
+    }
+
+    $element['add_more'] = array(
+      '#name' => strtr($id_prefix, '-', '_') . '_add_more',
+      '#type' => 'button',
+      '#value' => t('Add items'),
+      '#ajax' => array(
+        'callback' => array($this, 'ajaxCallback'),
+      ),
+      '#limit_validation_errors' => array(),
+      '#ervw_settings' => array(
+        'wrapper' => str_replace('_', '-', $field_name . '-values'),
+        'cardinality' => $cardinality,
+        'target_type' => $this->getFieldSetting('target_type'),
+      ),
+      '#element_validate' => array(array($this, 'elementValidate')),
+    );
+
+    $form['#attached']['library'][] = 'views/views.ajax';
+
     return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function elementValidate($element, FormStateInterface $form_state, $form) {
+    $form_state->setRebuild();
   }
 
   /**
@@ -137,37 +209,82 @@ class ViewWidget extends WidgetBase {
   }
 
   /**
-   * Ajax callback for the the add items button.
+   * Returns a renderable array of the rows.
+   *
+   * @param $entity_ids
    */
-  public function ajaxCallback(array $form, FormStateInterface &$form_state) {
-    $response = new AjaxResponse();
-    $view = $this->getSetting('view');
+  public static function getRows($entity_ids, $settings) {
+    $entity_ids = array_values($entity_ids);
+    $rows = array();
 
-    if (!empty($view)) {
-      list($view_id, $display_id) = explode('|', $view);
-      if ($view = Views::getView($view_id)) {
-        $view->setAjaxEnabled(TRUE);
-        $preview = $view->preview($display_id);
-        $response->addCommand(new SettingsCommand($view->element['#attached']['js'][0]['data'], TRUE));
-        $form_settings = array(
-          'view' => $view,
-          'preview' => $preview,
+    if (!empty($entity_ids)) {
+      $entities = entity_load_multiple($settings['target_type'], $entity_ids);
+      $max = count($entities);
+      $delta = 0;
+
+      foreach ($entities as $entity_id => $entity_item) {
+        $rows[] = array(
+          'target_id' => array(
+            '#type' => 'checkbox',
+            '#delta' => $delta,
+            '#field_suffix' => String::checkPlain($entity_item->label()),
+            '#return_value' => $entity_id,
+            '#value' => $entity_id,
+          ),
+          '_weight' => array(
+            '#type' => 'weight',
+            '#title' => t('Weight for row @number', array('@number' => $delta + 1)),
+            '#title_display' => 'invisible',
+            // Note: this 'delta' is the FAPI #type 'weight' element's property.
+            '#delta' => $max,
+            '#default_value' => $delta,
+            '#weight' => 100,
+          ),
         );
-        $modal_form = \Drupal::formBuilder()->getForm('Drupal\entity_reference_view_widget\Form\ModalForm', $form_settings);
-        $response->addCommand(new SettingsCommand($modal_form['actions']['add_items']['#attached']['js'][0]['data'], TRUE));
-        $modal_content = '';
-
-        // Display the exposed widgets on top of the modal.
-        if (!empty($view->exposed_widgets)) {
-          $modal_content .= drupal_render($view->exposed_widgets);
-        }
-        $modal_content .= drupal_render($modal_form);
-
-        $response->addCommand(new OpenModalDialogCommand($view->storage->label(), $modal_content, array('width' => 700)));
+        $delta++;
       }
     }
 
-    return $response;
+    return $rows;
+  }
+
+  /**
+   * Ajax callback for the the add items button.
+   */
+  public function ajaxCallback(array $form, FormStateInterface &$form_state) {
+    $triggering_element = $form_state->getTriggeringElement();
+    if (isset($triggering_element['#ervw_settings'])) {
+      $response = new AjaxResponse();
+      $view = $this->getSetting('view');
+
+      if (!empty($view)) {
+        list($view_id, $display_id) = explode('|', $view);
+        if ($view = Views::getView($view_id)) {
+          $view->setAjaxEnabled(TRUE);
+          $preview = $view->preview($display_id);
+          $response->addCommand(new SettingsCommand($view->element['#attached']['js'][0]['data'], TRUE));
+          $form_settings = array(
+            'view' => $view,
+            'preview' => $preview,
+            'settings' => $triggering_element['#ervw_settings'] + $this->getSettings(),
+          );
+          $modal_form = \Drupal::formBuilder()
+            ->getForm('Drupal\entity_reference_view_widget\Form\ModalForm', $form_settings);
+          $response->addCommand(new SettingsCommand($modal_form['actions']['add_items']['#attached']['js'][0]['data'], TRUE));
+          $modal_content = '';
+
+          // Display the exposed widgets on top of the modal.
+          if (!empty($view->exposed_widgets)) {
+            $modal_content .= drupal_render($view->exposed_widgets);
+          }
+          $modal_content .= drupal_render($modal_form);
+
+          $response->addCommand(new OpenModalDialogCommand($view->storage->label(), $modal_content, array('width' => 700)));
+        }
+      }
+
+      return $response;
+    }
   }
 
 }
